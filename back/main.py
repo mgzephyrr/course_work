9b93ac2998b796c6c6d65de80a53ced7cdbe8242
misha <mllevin@edu.hse.ru>
2024-04-07 21:04:31 +0500

    1234

--- back/main.py
+++ back/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from back.User.router import router as router_users
 from back.SystemRole.router import router as router_roles
 from back.Event.router import router as router_events
@@ -10,3 +11,17 @@ app = FastAPI(
 app.include_router(router_roles)
 app.include_router(router_users)
 app.include_router(router_events)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
