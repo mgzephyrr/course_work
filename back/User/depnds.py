2a0444f8df0216ccb6fec16bd507b07563d82fdb
misha <mllevin@edu.hse.ru>
2024-04-10 22:19:00 +0500

    cookie + email fix

---
+++ back/User/depnds.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timezone
+from fastapi import Depends, HTTPException, Request, status
+from jose import jwt, JWTError
+from jwt.exceptions import JWTException
+
+from back import crud
+from back.config import settings
+
+
+def get_token(request: Request):
+    token = request.cookies.get("Authorization")
+    if not token:
+        raise HTTPException(status_code=401, detail="Token absent")
+    return token
+
+async def get_current_user(token: str = Depends(get_token)):
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, settings.ALGORITHM
+        )
+        user = await crud.get_user_by_email(email=payload.get("sub"))
+        if user is None:
+            raise HTTPException(status_code=401, detail="Invalid user")
+        return user
+    except JWTException:
+        raise HTTPException(status_code=401, detail="Could not validate credentials")
--- back/User/router.py
+++ back/User/router.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 import re
-from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
+from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, Response, Cookie
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jwt.exceptions import JWTException
 from jose import jwt
@@ -9,7 +9,9 @@ import uuid
 from PIL import Image
 from fastapi.responses import FileResponse
 
+from back.User.depnds import get_current_user
 from back.Token.schemas import Token
+from back.User.models import User
 from back.User.schemas import SUser, SUserCreate
 from back.images_upload.repo import upload_image
 from back import auth
@@ -20,28 +22,33 @@ router = APIRouter(
     tags= ["Аутентификация", "Работа с пользователем"]
 )
 
-email_regex = r'^[a-zA-Z0-9_.+-]+@edu\.hse\.ru$' or r'^[a-zA-Z0-9_.+-]+@hse\.ru$'
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
-
+email_regex = r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
 
 @router.post("/signup")
-async def create_user(user: SUserCreate, system_role_id: int = 3) -> SUser:
+async def create_user(user: SUserCreate) -> SUser:
     if re.match(email_regex, user.email):
-        # !!!!!!!!! Если почта заканчивается на @edu.hse.ru то выдать роль student
-        db_role = await crud.get_system_role(system_role_id = system_role_id)
+        system_role_id = None
+        if user.email.endswith("@edu.hse.ru"):
+            system_role_id = 5
+        elif user.email.endswith("@hse.ru"):
+            system_role_id = 6
+        else:
+            system_role_id = 3
+
+        db_role = await crud.get_system_role(system_role_id=system_role_id)
         if not db_role:
             raise HTTPException(status_code=400, detail="Role does not exist")
+
         db_user = await crud.get_user_by_email(email=user.email)
         if db_user:
             raise HTTPException(status_code=400, detail="Email already in use")
 
         return await crud.create_user(user=user, system_role_id=system_role_id)
     else:
-        raise HTTPException(status_code=400, detail="Only email addresses ending with @edu.hse.ru are allowed")
+        raise HTTPException(status_code=400, detail="Invalid email format")
 
 @router.post("/login", response_model=Token)
-async def login_for_access_token(from_data: OAuth2PasswordRequestForm = Depends()):
+async def login_for_access_token(response: Response, from_data: OAuth2PasswordRequestForm = Depends()):
     print(from_data)
     db_user = await crud.get_user_by_email(email=from_data.username)
     if not db_user or not auth.verify_password(from_data.password, db_user.hashed_password):
@@ -50,23 +57,17 @@ async def login_for_access_token(from_data: OAuth2PasswordRequestForm = Depends(
     access_token = auth.create_access_token(
         data={"sub": db_user.email}, expires_delta=access_token_expires
     )
+    response.set_cookie(key="Authorization", value= access_token, httponly=True)
     return {"access_token": access_token, "token_type": "bearer"}
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(
-            token, settings.SECRET_KEY, settings.ALGORITHM
-        )
-        user = await crud.get_user_by_email(email=payload.get("sub"))
-        if user is None:
-            raise HTTPException(status_code=401, detail="Invalid user")
-        return user
-    except JWTException:
-        raise HTTPException(status_code=401, detail="Could not validate credentials")
+@router.post("/logout")
+async def logout(response: Response):
+    response.delete_cookie("Authorization")
+    return {"message": "Successfully logged out"}
 
-@router.get("/protected")
-async def protected_route(user: SUser = Depends(get_current_user)):
-    return {"message": "Hello, {}".format(user.email)}
+@router.get("/me")
+async def protected_route(user: User = Depends(get_current_user)):
+    return user
 
 @router.post("/createavatar")
 async def create_upload_file(file: UploadFile = File(...), user: SUser = Depends(get_current_user)):
